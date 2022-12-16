88c995e3e0b4303a5cfbbb56e764debf219146e7
idil <idil.iman1999@gmail.com>
2022-12-16 09:40:31 +0800

    workstation changed

--- pages/checkout.js
+++ pages/checkout.js
@@ -4,9 +4,12 @@ import { useSelector } from "react-redux";
 import CheckoutProduct from "../src/components/CheckoutProduct";
 import Header from "../src/components/Header";
 import { selectItems } from "../src/slices/basketSlice";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
 
 function Checkout() {
   const items = useSelector(selectItems);
+  const { data: session, status } = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -43,7 +46,28 @@ function Checkout() {
         </div>
 
         {/* Right */}
-        <div></div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length} items):
+                <span className="font-bold">
+                  <Currency currency="MYR" />
+                </span>
+              </h2>
+
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !session &&
+                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                }`}
+              >
+                {!session ? "Sign in to checkout" : "Proceed to checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
